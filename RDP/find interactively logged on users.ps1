624c3ec2d96341214ab3588faee1b0e26a48ea6b
Thiago Pilz <49417494+thiagopilz@users.noreply.github.com>
2021-12-21 12:41:03 -0300

    Update find interactively logged on users.ps1

--- RDP/find interactively logged on users.ps1
+++ RDP/find interactively logged on users.ps1
@@ -15,8 +15,10 @@ Else
         Write-Host "$Domain\$Username logged on since: $($i.ConvertToDateTime($i.CreationDate))"
     }
 }
+#----
 
 
+# powershell-script-to-see-currently-logged-in-users-domain-and-machine-status
 #PS C:\tmp> .\teste.ps1
 #COMPUS\ncardoso logged on since: 08/05/2021 17:47:41
 #COMPUS\postojb_01 logged on since: 08/06/2021 07:59:28
@@ -24,8 +26,3 @@ Else
 #COMPUS\postosbo_01 logged on since: 08/09/2021 09:33:42
 #COMPUS\tpilz logged on since: 08/09/2021 18:40:54
 #PS C:\tmp>
-
-
-
-
-# https://stackoverflow.com/questions/23219718/powershell-script-to-see-currently-logged-in-users-domain-and-machine-status
